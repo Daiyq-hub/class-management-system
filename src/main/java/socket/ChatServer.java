ce2d4addf5d4929e48ac7318a6931200558e51d8
Daiyq-hub <3086780092@qq.com>
2025-10-31 00:56:47 +0800

    Refactor ChatServer to use WebSocket instead of Socket

--- src/main/java/socket/ChatServer.java
+++ src/main/java/socket/ChatServer.java
@@ -1,21 +1,44 @@
+package socket;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@ServerEndpoint("/chat/{username}")
 public class ChatServer {
-    public static void main(String[] args) throws IOException {
-        // 或集成到Spring Boot
-        ServerSocket server = new ServerSocket(8081);
-        Socket client = server.accept();
-        new Thread(() -> { // 读线程
-            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            while (true) {
-                System.out.println(in.readLine());
+    
+    private static Map<String, Session> clients = new ConcurrentHashMap<>();
+    
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) throws IOException {
+        clients.put(username, session);
+        broadcastMessage(username + " 加入了聊天室！");
+    }
+    
+    @OnMessage
+    public void onMessage(String message, @PathParam("username") String username) throws IOException {
+        broadcastMessage(username + ": " + message);
+    }
+    
+    @OnClose
+    public void onClose(@PathParam("username") String username) throws IOException {
+        clients.remove(username);
+        broadcastMessage(username + " 离开了聊天室！");
+    }
+    
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+    
+    private void broadcastMessage(String message) throws IOException {
+        for (Session session : clients.values()) {
+            if (session.isOpen()) {
+                session.getBasicRemote().sendText(message);
             }
-        }).start();
-        new Thread(() -> { // 写线程
-            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-            Scanner scanner = new Scanner(System.in);
-            while (true) {
-                out.println(scanner.nextLine());
-            }
-        }).start();
+        }
     }
 }
-// 客户端类似，使用Socket连接IP:port
